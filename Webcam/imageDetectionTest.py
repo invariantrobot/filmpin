8ae08e9744e7e0eebb622c324953cd3bd1993935
Osvald Svenaeus <osvald.skolan@hotmail.com>
2025-10-29 12:49:56 +0100

    image detection and frame implemented

--- Webcam/imageDetectionTest.py
+++ Webcam/imageDetectionTest.py
@@ -1,5 +1,9 @@
 import cv2
 
+def hotSpot(matches, dimensions):
+    
+
+
 
 cam = cv2.VideoCapture(0)
 
@@ -10,6 +14,7 @@ orb = cv2.ORB_create()
 BORDER_SIZE = 10
 OFF_WHITE = (227, 238, 246)
 OPACITY = 0.8
+HOTSPOT_THRESHOLD = 4
 
 overlay_img = cv2.resize(cv2.imread("Pen.png"), (315, 89))
 
@@ -42,35 +47,46 @@ pos_w_1 -= (pos_w_1 - pos_w_0) - overlay_w
 pos_h_1 -= (pos_h_1 - pos_h_0) - overlay_h
 
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+image_detection = False
 
 
 while True:
     ret, frame = cam.read()
 
     overlay = frame.copy()
-    kp1, des1 = orb.detectAndCompute(frame, None)
-    kp2, des2 = orb.detectAndCompute(overlay_img, None)
-
-    frame = cv2.drawKeypoints(frame, kp1, None, color=(0, 255, 0))
-    overlay_img_2 = cv2.drawKeypoints(overlay_img, kp2, None, color=(0, 255, 0))
-
-    matches = bf.match(des1, des2)
-
-    matches = sorted(matches, key=lambda x: x.distance)
     
     H, W = frame.shape[:2]
     
     if not ret:
         print("failed to grab frame")
         break
+    kp1, des1 = orb.detectAndCompute(frame, None)
+    kp2, des2 = orb.detectAndCompute(overlay_img, None)
+    
+
+
+    matches = bf.match(des1, des2)
+    matches = sorted(matches, key=lambda x: x.distance)
+    iter = 0
+
+    for match in matches:
+        p1 = kp1[match.queryIdx].pt
+        point = (round(p1[0]),round(p1[1]))
+        cv2.circle(frame, center = point, radius = 10, color =(0,255,0), thickness=2)
+        iter += 1
+        if iter > 10:
+            break
+    
+        
     
     # Take out image part and blend with image
     smallerFrame = overlay[pos_w_0:pos_w_1, pos_h_0:pos_h_1]
 
 
-    blendedFrame = cv2.addWeighted(smallerFrame, 1 - OPACITY, overlay_img_2, OPACITY, 0)
+    blendedFrame = cv2.addWeighted(smallerFrame, 1 - OPACITY, overlay_img, OPACITY, 0)
 
     frame[pos_w_0:pos_w_1, pos_h_0:pos_h_1] = blendedFrame
+
     
     cv2.imshow("test", frame)
 
@@ -80,6 +96,7 @@ while True:
         print("Escape hit, closing...")
         break
     elif k%256 == 32:
+        image_detection = True
         # SPACE pressed
         img_name = "opencv_frame_{}.png".format(img_counter)
         cv2.imwrite(img_name, frame)
